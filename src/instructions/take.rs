97ff670f34eedf2ba761daa617217cc2d7c4757d
Fernando Ribas <fribas84@gmail.com>
2026-09-15 22:50:58 -0300

    Enhance Take instruction processing
    - Refactored the `process_take_instruction` function to improve validation checks for taker and escrow ownership.
    - Added logic to create associated token accounts if they do not exist.
    - Implemented transfer operations for both the taker and the vault, ensuring proper authority checks.
    - Included functionality to close the vault and escrow accounts after the transaction is completed.

--- src/instructions/take.rs
+++ src/instructions/take.rs
@@ -1,34 +1,138 @@
 use pinocchio::{
     cpi::{Seed, Signer},
     error::ProgramError,
-    sysvars::{rent::Rent, Sysvar},
     AccountView, Address, ProgramResult,
 };
-use pinocchio_system::instructions::CreateAccount;
 
 use crate::state::Escrow;
 
-/// 8 (amount_to_receive) + 8 (amount_to_give)
-const MAKE_DATA_LEN: usize = 16;
-
-pub fn process_take_instruction(accounts: &mut [AccountView], data: &[u8]) -> ProgramResult {
-    let [
-        taker,
-        maker,
-        mint_a,
-        mint_b,
-        escrow_account,
-        vault,
-        taker_ata_a,
-        taker_ata_b,
-        maker_ata_b,
-        system_program,
-        token_program,
-        _associated_token_program] =
+pub fn process_take_instruction(accounts: &mut [AccountView], _data: &[u8]) -> ProgramResult {
+    let [taker, maker, mint_a, mint_b, escrow_account, vault, taker_ata_a, taker_ata_b, maker_ata_b, system_program, token_program, _associated_token_program] =
         accounts
     else {
         return Err(ProgramError::NotEnoughAccountKeys);
     };
 
+    // Validate if taker a user
+    if !taker.is_signer() {
+        return Err(ProgramError::MissingRequiredSignature);
+    }
 
+    // Validate if this escrow owns the account
+    if !escrow_account.owned_by(&crate::ID) {
+        return Err(ProgramError::IllegalOwner);
+    }
+
+    let (amount_to_receive, bump) = {
+        let escrow = Escrow::load_mut(escrow_account)?;
+
+        if escrow.maker() != *maker.address() {
+            return Err(ProgramError::InvalidAccountData);
+        }
+        if escrow.mint_a() != *mint_a.address() {
+            return Err(ProgramError::InvalidAccountData);
+        }
+        if escrow.mint_b() != *mint_b.address() {
+            return Err(ProgramError::InvalidAccountData);
+        }
+        (escrow.amount_to_receive(), escrow.bump)
+    };
+
+    let escrow_pda = Address::from(pinocchio_pubkey::derive_address(
+        &[b"escrow", maker.address().as_ref()],
+        Some(bump),
+        &crate::ID.to_bytes(),
+    ));
+
+    if escrow_pda != *escrow_account.address() {
+        return Err(ProgramError::InvalidSeeds);
+    }
+
+    let vault_amount = {
+        let vault_state = pinocchio_token::state::Account::from_account_view(vault)?;
+        if vault_state.owner() != escrow_account.address() {
+            return Err(ProgramError::IllegalOwner);
+        }
+        if vault_state.mint() != mint_a.address() {
+            return Err(ProgramError::InvalidAccountData);
+        }
+        vault_state.amount()
+    };
+
+    // CREATE PDAS IF THEY DONT EXISTS
+    pinocchio_associated_token_account::instructions::CreateIdempotent {
+        funding_account: taker,
+        account: taker_ata_a,
+        wallet: taker,
+        mint: mint_a,
+        system_program,
+        token_program,
+    }
+    .invoke()?;
+
+    pinocchio_associated_token_account::instructions::CreateIdempotent {
+        funding_account: taker,
+        account: maker_ata_b,
+        wallet: maker,
+        mint: mint_b,
+        system_program,
+        token_program,
+    }
+    .invoke()?;
+
+    // validate that accounts exists
+
+    {
+        let taker_ata_b_state = pinocchio_token::state::Account::from_account_view(taker_ata_b)?;
+        if taker_ata_b_state.owner() != taker.address() {
+            return Err(ProgramError::IllegalOwner);
+        }
+        if taker_ata_b_state.mint() != mint_b.address() {
+            return Err(ProgramError::InvalidAccountData);
+        }
+    }
+
+    // Taker pays Maker
+    pinocchio_token::instructions::Transfer {
+        from: taker_ata_b,
+        to: maker_ata_b,
+        authority: taker,
+        multisig_signers: &[] as &[&AccountView],
+        amount: amount_to_receive,
+    }
+    .invoke()?;
+
+    // Vault pays to Taker
+
+    let bump_bytes = [bump];
+    let seed = [
+        Seed::from(b"escrow"),
+        Seed::from(maker.address().as_array()),
+        Seed::from(&bump_bytes),
+    ];
+    let signer = Signer::from(&seed);
+
+    pinocchio_token::instructions::Transfer {
+        from: vault,
+        to: taker_ata_a,
+        authority: escrow_account,
+        multisig_signers: &[] as &[&AccountView],
+        amount: vault_amount,
+    }
+    .invoke_signed(&[signer.clone()])?;
+
+    //close vualt, rent to Maker
+    pinocchio_token::instructions::CloseAccount {
+        account: vault,
+        destination: maker,
+        authority: escrow_account,
+        multisig_signers: &[] as &[&AccountView],
+    }
+    .invoke_signed(&[signer.clone()])?;
+
+    // close escrow
+    maker.set_lamports(maker.lamports() + escrow_account.lamports());
+    escrow_account.set_lamports(0);
+    escrow_account.close()?;
+    Ok(())
 }
