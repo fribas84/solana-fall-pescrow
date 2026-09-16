64f7da10f1c133cafb2b358d7da57f2396bcce11
Fernando Ribas <fribas84@gmail.com>
2026-09-15 22:30:41 -0300

    Add Take instruction processing
    - Implemented the `process_take_instruction` function in `take.rs` to handle the Take instruction.
    - Updated the instruction module to include the new Take instruction and its processing function.
    - Modified the main instruction processing logic in `lib.rs` to route Take instructions appropriately.

--- src/instructions/mod.rs
+++ src/instructions/mod.rs
@@ -1,6 +1,9 @@
 pub mod make;
+pub mod take;
 
 pub use make::*;
+pub use take::*;
+
 use pinocchio::error::ProgramError;
 
 pub enum EscrowInstructions {
--- src/instructions/take.rs
+++ src/instructions/take.rs
@@ -1,5 +1,8 @@
 use pinocchio::{
-    AccountView, Address, ProgramResult, cpi::{Seed, Signer}, error::ProgramError, sysvars::{Sysvar, rent::Rent}
+    cpi::{Seed, Signer},
+    error::ProgramError,
+    sysvars::{rent::Rent, Sysvar},
+    AccountView, Address, ProgramResult,
 };
 use pinocchio_system::instructions::CreateAccount;
 
@@ -8,8 +11,24 @@ use crate::state::Escrow;
 /// 8 (amount_to_receive) + 8 (amount_to_give)
 const MAKE_DATA_LEN: usize = 16;
 
-pub fn process_make_instruction(
-    accounts: &mut [AccountView],
-    data: &[u8],
-) -> ProgramResult {
+pub fn process_take_instruction(accounts: &mut [AccountView], data: &[u8]) -> ProgramResult {
+    let [
+        taker,
+        maker,
+        mint_a,
+        mint_b,
+        escrow_account,
+        vault,
+        taker_ata_a,
+        taker_ata_b,
+        maker_ata_b,
+        system_program,
+        token_program,
+        _associated_token_program] =
+        accounts
+    else {
+        return Err(ProgramError::NotEnoughAccountKeys);
+    };
+
+
 }
